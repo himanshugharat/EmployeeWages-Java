410b9420797167c77a65ee5ea187b30fcdc0ecf3
himanshugharat <himanshugharat44@gmail.com>
2020-07-17 10:09:24 -0500

    ADDED:calculated Employee Wages

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,12 +1,18 @@
 class EmpWageComputation {
 	public static void main(String args[]) {
-		// CONSTANTS
+
 		int IS_FULL_TIME = 1;
-		//COMPUTATION
+		int EMP_RATE_PER_HOUR = 20;
+		int empHrs = 0;
+		int empWage = 0;
+
 		int empCheck = (int) ( Math.random() * 10 ) % 2;
 		System.out.println(empCheck);
 		if ( empCheck == IS_FULL_TIME )
-			System.out.println( "Emplyee is Present" );
+			empHrs = 8;
 		else
-			System.out.println( "Employee is Absent" );
+			empHrs = 0;
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Employee Wage: " + empWage);
+}
 }
