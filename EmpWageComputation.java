4536e5ae0b82c78ac3dcc355f826a4ea432e258c
himanshugharat <himanshugharat44@gmail.com>
2020-07-17 10:56:42 -0500

    ADDED:getter for variables

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,19 +1,41 @@
 public class EmpWageComputation {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
-	public static final int EMP_RATE_PER_HOUR ;
-	public static final int NUM_OF_WORKING_DAYS;
-	public static final int MAX_HRS_IN_MONTH;
-	public EmpWageComputation( int EMP_RATE_PER_HOUR, int NUM_OF_WORKING_DAYS, int MAX_HRS_IN_MONTH )	{
+	public static int MAX_NUM_OF_WORKING_DAYS;
+	public static String COMPANY_NAME;
+	public static int EMP_RATE_PER_HOUR ;
+	public static int MAX_HRS_IN_MONTH;
+	public EmpWageComputation( int EMP_RATE_PER_HOUR, int MAX_ NUM_OF_WORKING_DAYS, int MAX_HRS_IN_MONTH )	{
 		this.EMP_RATE_PER_HOUR = EMP_RATE_PER_HOUR;
-		this.NUM_OF_WORKING_DAYS = NUM_OF_WORKING_DAYS;
+		this.MAX_NUM_OF_WORKING_DAYS = MAx_NUM_OF_WORKING_DAYS;
 		this.MAX_HRS_IN_MONTH = MAX_HRS_IN_MONTH;
 	}
-public static int computeTotalEmpHrs( )	{
+	public static String getCompanyName()	{
+		return COMPANY_NAME;
+	}
+
+	public static int getEmpRatePerHour()	{
+		return EMP_RATE_PER_HOUR;
+	}
+
+	public static int getMaxNumOfWorkingDays()	{
+		return MAX_NUM_OF_WORKING_DAYS;
+	}
+
+	public static int getMaxHrsInMonth()	{
+		return MAX_HRS_IN_MONTH;
+	}
+
+	public static int getTotalEmpWage()	{
+		return TOTAL_EMP_WAGE;
+	}
+
+
+public static void computeTotalEmpWage( )	{
 		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 		int empCheck = (int) ( Math.random() * 10 ) % 2;
-		while ( totalEmpHrs <= MAX_HRS_IN_MONTH &&
-					totalWorkingDays < NUM_OF_WORKING_DAYS )	{
+		while ( totalEmpHrs <= getMaxHrsInMonth &&
+					totalWorkingDays < getNumOfWorkingDays )	{
 			totalWorkingDays++;
 		switch (empCheck)	{
 			case IS_PART_TIME:
@@ -29,25 +51,18 @@ public static int computeTotalEmpHrs( )	{
 		totalEmpHrs += empHrs;
 			System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " +  empHrs);
 }
-return totalEmpHrs;
+		TOTAL_EMP_WAGE = totalEmpHrs * getEmpRatePerHour();
+		System.out.println("Total Wage of " + getCompanyName() + " Employee is " + getTotalEmpWage());
 
 }
-public int calTotalEmpWage( int totalEmpHrs )	{
-		return totalEmpHrs * EMP_RATE_PER_HOUR;
-	}
 
 
 	public static void main(String args[]) {
 		EmpWageComputation companyA = new EmpWageComputation( 20, 20, 100 );
-		int totalCompanyAEmpWage = companyA.computerTotalEmpHrs( );
-		int totalCompanyAEmpWage = companyA.calTotalEmpWage( companyATotalEmpHrs );
-		System.out.println("Total Wage of CompanyA Employee: " + totalCompanyAEmpWage);
+		companyA.computerTotalEmpHrs( );
 
 		EmpWageComputation B = new EmpWageComputation( 50, 15, 75 );
-		int BTotalEmpHrs = B.computeTotalEmpHrs();
-		int totalBEmpWage = B.computeTotalEmpHrs( BTotalEmpHrs );
-		System.out.println("Total Wage of B Employee: " + totalBEmpWage );
-
+		B.computeTotalEmpHrs();
 
 }
 }
