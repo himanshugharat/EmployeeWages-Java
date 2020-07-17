5a531cdb11696855dc616777c505e6f051ecb50b
himanshugharat <himanshugharat44@gmail.com>
2020-07-17 10:12:09 -0500

    ADDED:part time employee

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,7 +1,8 @@
 class EmpWageComputation {
 	public static void main(String args[]) {
 
-		int IS_FULL_TIME = 1;
+		int IS_FULL_TIME = 2;
+		int IS_PART_TIME = 1;
 		int EMP_RATE_PER_HOUR = 20;
 		int empHrs = 0;
 		int empWage = 0;
@@ -10,8 +11,10 @@ class EmpWageComputation {
 		System.out.println(empCheck);
 		if ( empCheck == IS_FULL_TIME )
 			empHrs = 8;
+		else if(empCheck == IS_PART_TIME)
+			empHrs = 4;
 		else
-			empHrs = 0;
+			empHrs=0
 		empWage = empHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Employee Wage: " + empWage);
 }
